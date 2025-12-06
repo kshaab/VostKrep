af8b98c664848e769880a5ebbcba2749bf6cdd6a
Ксения <kssshaab@gmail.com>
2025-12-06 18:20:57 +0300

    add

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Welcome from "../components/Welcome";
+
+export default function Home() {
+  return <Welcome />;
+}
+
