5c2b6aa0a71f9072beac7a09cd131a508f1ad6a0
K <kssshaab@gmail.com>
2026-03-19 20:58:41 +0300

    correct23

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 
 const nextConfig = {
+  turbopack: {},
   reactStrictMode: true,
   webpack: (config: { resolve: { alias: { [x: string]: string; }; }; }) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
