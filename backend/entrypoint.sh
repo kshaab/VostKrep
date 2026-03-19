fa624368ad46673790e2503656c9a0279fcfdfca
K <kssshaab@gmail.com>
2026-03-19 22:30:06 +0300

    correct32

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Применяем миграции..."
-python /project/backend/manage.py migrate
+python /project/manage.py migrate
 
 echo "Собираем статические файлы..."
-python /project/backend/manage.py collectstatic --noinput
+python /project/manage.py collectstatic --noinput
 
 echo "Запускаем Gunicorn..."
 exec gunicorn src.project.wsgi:application --bind 0.0.0.0:8000 --workers 3
