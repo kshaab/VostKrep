5bfa6c56a0a4d779cec68f112ed630c0430cc69f
K <kssshaab@gmail.com>
2026-03-19 15:53:08 +0300

    correct8

--- backend/src/project/settings.py
+++ backend/src/project/settings.py
@@ -63,9 +63,8 @@ TEMPLATES = [
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
-CSRF_TRUSTED_ORIGINS = [
-    os.getenv("CSRF_TRUSTED_ORIGINS"),
-]
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 
 CORS_ALLOW_CREDENTIALS = True
 
