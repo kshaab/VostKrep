030fde496c77c47e205076d992450db616484b71
K <kssshaab@gmail.com>
2026-04-07 16:51:01 +0300

    correct homepage

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,9 +1,17 @@
 import HomePage from "@/components/pages/HomePage"
 import CategoriesSectionClient from "@/components/categories/CategoriesSectionClient"
 import CategoriesSectionSSR from "@/components/categories/CategoriesSectionSSR"
+import { Category } from "@/types/category"
 
 export default async function Home() {
-  const categories = await CategoriesSectionSSR() // SSR fetch
+  let categories: Category[] = []
+
+  try {
+    categories = await CategoriesSectionSSR()
+  } catch (error) {
+    console.error("SSR fetch failed:", error)
+    categories = [] // безопасно для сборки
+  }
 
   return (
     <main>
