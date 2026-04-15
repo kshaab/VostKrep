073a24a9ff34055a260422694ffdd41328c2dc98
K <kssshaab@gmail.com>
2026-04-15 14:42:07 +0300

    correct celery

--- backend/src/project/apps/order/services.py
+++ backend/src/project/apps/order/services.py
@@ -4,7 +4,6 @@ from project.apps.order.models import Order
 
 
 def send_telegram_order(order: Order) -> None:
-    """Отправляет сообщение о новой заявке в Телеграм-чат"""
     message = (
         f"Новая заявка #{order.id}\n\n"
         f"Имя: {order.name}\n"
@@ -20,18 +19,33 @@ def send_telegram_order(order: Order) -> None:
         for item in items:
             message += f"- {item.product_name} x {item.quantity} {item.unit}\n"
 
-    if order.file:
-        with open(order.file.path, "rb") as f:
-            requests.post(
-                f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendDocument",
-                data={"chat_id": settings.TELEGRAM_CHAT_ID, "caption": message},
-                files={"document": f},
+    try:
+        if order.file:
+            with open(order.file.path, "rb") as f:
+                response = requests.post(
+                    f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendDocument",
+                    data={
+                        "chat_id": settings.TELEGRAM_CHAT_ID,
+                        "caption": message,
+                    },
+                    files={"document": f},
+                    timeout=10,
+                )
+        else:
+            response = requests.get(
+                f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage",
+                params={
+                    "chat_id": settings.TELEGRAM_CHAT_ID,
+                    "text": message,
+                },
                 timeout=10,
             )
 
-    else:
-        requests.get(
-            f"{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage",
-            params={"chat_id": settings.TELEGRAM_CHAT_ID, "text": message},
-            timeout=10,
-        )
+        print("TELEGRAM STATUS:", response.status_code)
+        print("TELEGRAM RESPONSE:", response.text)
+
+        response.raise_for_status()
+
+    except Exception as e:
+        print("TELEGRAM ERROR:", str(e))
+        raise e
--- backend/src/project/apps/order/tasks.py
+++ backend/src/project/apps/order/tasks.py
@@ -10,12 +10,13 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def send_order_to_telegram(order_id: int) -> None:
-    """Celery задача для отправки заявки в Телеграм"""
-
     try:
         order = Order.objects.get(id=order_id)
+        send_telegram_order(order)
+
     except Order.DoesNotExist:
         logger.error(f"Заявка {order_id} не найдена")
-        return
 
-    send_telegram_order(order)
+    except Exception as e:
+        logger.error(f"Ошибка отправки в Telegram: {str(e)}")
+        raise e
